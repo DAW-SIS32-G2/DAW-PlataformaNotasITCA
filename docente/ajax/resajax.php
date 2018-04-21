d2fe6dfdbc65e58397ac6afc982aaebd42901862
José Marcelo Hernández Cerritos <jose.marcelo.1706@gmail.com>
2018-04-20 22:06:48 -0600

    Limpieza de repo
    Eliminé todos los archivos para poder adaptar el framework MVC

--- docente/ajax/resajax.php
+++
@@ -1,9 +0,0 @@
-<?php
-$carnet = $_POST['carnet'];
-?>
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-  <strong><?php echo $carnet; ?></strong> Se ha cambiado su contraseña.
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>
