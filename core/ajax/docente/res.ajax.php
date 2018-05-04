d7815aefec761d3e71c0ea11e20fd89d1d1ddea9
José Marcelo Hernández Cerritos <jose.marcelo.1706@gmail.com>
2018-05-03 21:15:33 -0600

    Restablecer contraseña alumno
    completada funcion de restablecimiento de claves en módulo docente

--- core/ajax/docente/res.ajax.php
+++ core/ajax/docente/res.ajax.php
@@ -1,9 +1,30 @@
 <?php
-$carnet = $_POST['carnet'];
+	$carnet = $_POST['carnet'];
+	require_once("core/funcionesbd.php");
+	require_once("core/criptografia.php");
+	$objBase = new funcionesBD();
+	$contra = cifrar("itca");
+	$respuesta = $objBase->ConsultaPersonalizada("UPDATE Usuario SET contra='$contra' WHERE carnet='$carnet'");
+	if($respuesta === TRUE)
+	{
+		?>
+		<div class="alert alert-success alert-dismissible fade show" role="alert">
+		  <strong><?php echo $carnet; ?></strong> Se ha cambiado su contraseña.
+		  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		    <span aria-hidden="true">&times;</span>
+		  </button>
+		</div>
+		<?php
+	}
+	else
+	{
+		?>
+		<div class="alert alert-danger alert-dismissible fade show" role="alert">
+		  <strong><?php echo $carnet; ?></strong> Ha ocurrido un error.
+		  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		    <span aria-hidden="true">&times;</span>
+		  </button>
+		</div>
+		<?php
+	}
 ?>
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-  <strong><?php echo $carnet; ?></strong> Se ha cambiado su contraseña.
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>
