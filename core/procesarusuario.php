e4a28250079d1ef658fb219b8653d617c62fc6de
DMGarcia17 <daniel.moreno17101998@gmail.com>
2018-05-24 14:20:53 -0600

    Fase de comentado de archivos numero uno

--- core/procesarusuario.php
+++ core/procesarusuario.php
@@ -1,58 +1,57 @@
 <?php
-  session_start();
-  @define("__ROOT__", dirname(dirname(__FILE__)));
-  require_once __ROOT__."/config/variables.php";
-  require_once __ROOT__."/config/bd.php";
-  require_once __ROOT__."/core/funcionesbd.php";
+/**
+ * Este archivo sera el encargado de procesar el ingreso de cualquier usuario (docente o alumno) al sistema,
+ * Además define muchas variables de tipo SESSION muy importantes para el sistema
+ */
+
+# Se inicia la sesión
+session_start();
+
+# Se redefine la constante root
+@define("__ROOT__", dirname(dirname(__FILE__)));
+require_once __ROOT__ . "/config/variables.php";
+require_once __ROOT__ . "/config/bd.php";
+require_once __ROOT__ . "/core/funcionesbd.php";
 
 
-  //Carnet que servirá para hacer la consulta en la BD
-  $carnet = $_POST['carnet'];
+//Carnet que servirá para hacer la consulta en la BD
+$carnet = $_POST['carnet'];
 
-  if($_POST['enviar'] == "docente")
-  {
+if ($_POST['enviar'] == "docente") {
     $carnet = $_POST['carnet'];
-		$pass = $_POST['pass'];
-		$conn = new funcionesBD();
-		$mensaje = $conn->logueo($carnet,$pass,'docente');
-    if($mensaje == 1)
-    {
-      $_SESSION['usuario'] = $_POST['carnet'];
-      $_SESSION['tipo'] = "docente";
-      header("location: ".urlBase."docente");
+    $pass = $_POST['pass'];
+    $conn = new funcionesBD();
+    $mensaje = $conn->logueo($carnet, $pass, 'docente');
+    if ($mensaje == 1) {
+        $_SESSION['usuario'] = $_POST['carnet'];
+        $_SESSION['tipo'] = "docente";
+        header("location: " . urlBase . "docente");
+    } else {
+        unset($_SESSION['usuario']);
+        ?>
+        <script type="text/javascript">
+            alert("<?php echo $mensaje ?> Sus datos no son correctos");
+            window.location.replace("<?= urlBase ?>");
+        </script>
+        <?php
     }
-    else
-    {
-      unset($_SESSION['usuario']);
-      ?>
-      <script type="text/javascript">
-          alert("<?php echo $mensaje ?> Sus datos no son correctos");
-          window.location.replace("<?= urlBase ?>");
-      </script>
-      <?php
-    }
-  }
-  else
-  {
+} else {
     $carnet = $_POST['carnet'];
-		$pass = $_POST['pass'];
-		$conn = new funcionesBD();
-		$mensaje = $conn->logueo($carnet,$pass,'Usuario');
-    if($mensaje == 1)
-    {
-      $_SESSION['usuario'] = $_POST['carnet'];
-      $_SESSION['tipo'] = "alumno";
-      header("location: ".urlBase."alumno");
+    $pass = $_POST['pass'];
+    $conn = new funcionesBD();
+    $mensaje = $conn->logueo($carnet, $pass, 'Usuario');
+    if ($mensaje == 1) {
+        $_SESSION['usuario'] = $_POST['carnet'];
+        $_SESSION['tipo'] = "alumno";
+        header("location: " . urlBase . "alumno");
+    } else {
+        unset($_SESSION['usuario']);
+        ?>
+        <script type="text/javascript">
+            alert("Sus datos no son correctos");
+            window.location.replace("<?= urlBase ?>");
+        </script>
+        <?php
     }
-    else
-    {
-      unset($_SESSION['usuario']);
-      ?>
-      <script type="text/javascript">
-          alert("Sus datos no son correctos");
-          window.location.replace("<?= urlBase ?>");
-      </script>
-      <?php
-    }
-  }
+}
 ?>
--- core/zip.php
+++ core/zip.php
@@ -1,53 +1,53 @@
 <?php
 function comprimir($folder)
 {
-  // Get real path for our folder
-  $rootPath = realpath($folder);
-  echo $rootPath;
-  // Initialize archive object
-  $zip = new ZipArchive();
-  $archivo = 'Practicas/temp/practicas.zip';
-  $zip->open($archivo, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+    # Se obtiene la ruta real para la carpeta
+    $rootPath = realpath($folder);
+    echo $rootPath;
+    # Se instancia un objeto ZipArchive
+    $zip = new ZipArchive();
+    $archivo = 'Practicas/temp/practicas.zip';
+    $zip->open($archivo, ZipArchive::CREATE | ZipArchive::OVERWRITE);
 
-  // Create recursive directory iterator
-  /** @var SplFileInfo[] $files */
-  $files = new RecursiveIteratorIterator(
-      new RecursiveDirectoryIterator($rootPath),
-      RecursiveIteratorIterator::LEAVES_ONLY
-  );
+    // Create recursive directory iterator
+    /** @var SplFileInfo[] $files */
+    $files = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($rootPath),
+        RecursiveIteratorIterator::LEAVES_ONLY
+    );
 
-  foreach ($files as $name => $file)
-  {
-      // Skip directories (they would be added automatically)
-      if (!$file->isDir())
-      {
-          // Get real and relative path for current file
-          $filePath = $file->getRealPath();
-          $relativePath = substr($filePath, strlen($rootPath) + 1);
+    foreach ($files as $name => $file) {
+        // Skip directories (they would be added automatically)
+        if (!$file->isDir()) {
+            # Se obtiene la ruta real y la ruta relativa del directorio
+            $filePath = $file->getRealPath();
+            $relativePath = substr($filePath, strlen($rootPath) + 1);
 
-          // Add current file to archive
-          $zip->addFile($filePath, $relativePath);
-      }
-  }
+            # Se agregan los archivos al directorio
+            $zip->addFile($filePath, $relativePath);
+        }
+    }
 
-  // Zip archive will be created only after closing object
-  $zip->close();
-  return $archivo;
+    # Se crea el archivo una vez se haya cerrado el objeto
+    $zip->close();
+    return $archivo;
 }
+
 /**
-     * Download file
-     *
-     * @param string $path
-     * @param string $type
-     * @param string $name
-     * @param bool $force_download
-     * @return bool
-     */
-function download($path, $name = '', $type = 'application/octet-stream', $force_download = true) {
+ * Download file
+ *
+ * @param string $path
+ * @param string $type
+ * @param string $name
+ * @param bool $force_download
+ * @return bool
+ */
+function download($path, $name = '', $type = 'application/octet-stream', $force_download = true)
+{
 
-    if (!is_file($path) || connection_status() !== 0);
+    if (!is_file($path) || connection_status() !== 0) ;
 
-    if($force_download) {
+    if ($force_download) {
         header("Cache-Control: public");
     } else {
         header("Cache-Control: no-store, no-cache, must-revalidate");
@@ -55,29 +55,30 @@ function download($path, $name = '', $type = 'application/octet-stream', $force_
         header("Pragma: no-cache");
     }
 
-    header("Expires: ".gmdate("D, d M Y H:i:s", mktime(date("H")+2, date("i"), date("s"), date("m"), date("d"), date("Y")))." GMT");
-    header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+    header("Expires: " . gmdate("D, d M Y H:i:s", mktime(date("H") + 2, date("i"), date("s"), date("m"), date("d"), date("Y"))) . " GMT");
+    header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
     header("Content-Type: $type");
-    header("Content-Length: ".(string)(filesize($path)));
+    header("Content-Length: " . (string)(filesize($path)));
 
     $disposition = $force_download ? 'attachment' : 'inline';
 
-    if(trim($name) == '') {
+    if (trim($name) == '') {
         header("Content-Disposition: $disposition; filename=" . basename($path));
     } else {
-        header("Content-Disposition: $disposition; filename=\"" . trim($name)."\"");
+        header("Content-Disposition: $disposition; filename=\"" . trim($name) . "\"");
     }
 
     header("Content-Transfer-Encoding: binary\n");
 
     if ($file = fopen($path, 'rb')) {
-        while(!feof($file) and (connection_status()==0)) {
-            print(fread($file, 1024*8));
+        while (!feof($file) and (connection_status() == 0)) {
+            print(fread($file, 1024 * 8));
             flush();
         }
         fclose($file);
     }
 
-    return((connection_status() == 0) && !connection_aborted());
-} 
+    return ((connection_status() == 0) && !connection_aborted());
+}
+
 ?>
