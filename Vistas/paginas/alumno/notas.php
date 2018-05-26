b59bab172bf795be33b286231dfea6b9a1ce5dd0
José Marcelo Hernández Cerritos <jose.marcelo.1706@gmail.com>
2018-05-25 19:42:33 -0600

    Corrección de RegistrarAlumno

--- Vistas/paginas/alumno/notas.php
+++ Vistas/paginas/alumno/notas.php
@@ -1,22 +1,52 @@
+<?php
+require_once("core/funcionesbd.php");
+$sql = "SELECT CONCAT(nombres,' ',apellidos) as estudiante, foto FROM usuario WHERE carnet='".$_SESSION['usuario']."'";
+$objBD = new funcionesBD();
+$res = $objBD->ConsultaPersonalizada($sql);
+while($fila = mysqli_fetch_assoc($res))
+{
+    $nombreAlumno = $fila['estudiante'];
+    $foto = $fila['foto'];
+}
+?>
 <div class="container" style="padding-top:65px;">
     <div class="row">
         <div class="col-md-2">
             <div class="card" style="border-color: red;">
                 <div class="card-body">
-                    <img src="<?= imagenes ?>foto.png" alt="Foto" class="img-fluid">
+                    <?php
+                            if($datos['foto'] != "")
+                            {
+                                ?>
+                                <img src="<?= $datos['foto'] ?>" alt="Foto" class="img-fluid">
+                                <?php
+                            }
+                            else
+                            {
+                                ?>
+                                <img src="<?= imagenes ?>foto.png" alt="Foto" class="img-fluid">
+                                <?php
+                            }
+                        ?>
                 </div>
             </div>
         </div>
         <div class="col-md-10">
-            <h3>Nombre del Estudiante: <!--poner el nombre con php aqui--></h3>
-            <h4>Carnet: <!--poner el numero de carnet con php aqui--></h4>
+            <h3>Nombre del Estudiante: <?= $nombreAlumno ?></h3>
+            <h4>Carnet: <?= $_SESSION['usuario'] ?></h4>
             <form action="">
                 <label>Materia:
                     <select name="" id="" class="form-control">
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
+                        <option value="">--Seleccione Uno--</option>
+                        <?php
+                        $sql = "Select Modulo.idModulo, Modulo.nombreModulo, Modulo.tipoModulo from UsuarioActivo INNER JOIN Modulo ON UsuarioActivo.idModulo = Modulo.idModulo Where UsuarioActivo.carnet = '".$_SESSION['usuario']."'";
+                        $objBD = new funcionesBD();
+                        $res = $objBD->ConsultaPersonalizada($sql);
+                        while($fila = mysqli_fetch_assoc($res))
+                        {
+                            echo "<option value='".$fila['idModulo']."'>".$fila['nombreModulo']." (".$fila['tipoModulo'].")</option>";
+                        }
+                        ?>
                     </select>
                 </label>
             </form>
